77c73403a51fb07c1680f4e173e1c598d4eaf0fc
김대린 <62937325+kimdaerin@users.noreply.github.com>
2022-08-15 18:54:29 +0900

    Update post.js

--- models/post.js
+++ models/post.js
@@ -1,8 +1,8 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
+// like 포함 안한 버전, 이미지 
+"use strict";
+const { Model } = require(sequelize) 
+
+module.exports = (sequelize, DataTYpes) => {
   class Post extends Model {
     /**
      * Helper method for defining associations.
@@ -10,24 +10,27 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      // define associate here
     }
   }
-  Post.init({
-    postId: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: DataTypes.INTEGER
+  Post.init(
+    {
+      _id: {
+        primarykey: true,
+        allowNULL: false,
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+      },
+      userId: DataTypes.INTEGER,
+      nickname: Datatypes.STRING,
+      title:DataTypes.STRING,
+      content:DataTypes.STRING,
+      email: DataTypes.STRING,
     },
-    title: DataTypes.STRING,
-    content: DataTypes.STRING,
-    email: DataTypes.STRING,
-    userName : DataTypes.STRING,
-    like : DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Post',
-  });
+    {
+      sequelize,
+      modelName: "post",
+    }
+  );
   return Post;
 };
