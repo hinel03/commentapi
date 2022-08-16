fbf9d9e222b857a8b1b9302629c76a71d0667204
rmadbstjd <58474431+rmadbstjd@users.noreply.github.com>
2022-08-16 13:08:06 +0900

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -3,16 +3,14 @@ const express = require("express");
 const router = express.Router();
 
 //미들웨어 임포트
-const Auth = require("../middlewares/auth-middleware"); // 유저인증
-const auth = new Auth();
-const authMiddleware = auth.authMiddleware; // 유저인증 미들웨어 임포트
+const authMiddleware = require("../middlewares/auth-middleware");
 const PostsController = require("../controllers/posts.controller"); // 컨트롤러 임포트
 const postsController = new PostsController(); 
 
 // 게시글 목록 조회
 router.get("/", postsController.getAllPosts);
 // 게시글 작성
-router.post("/", authMiddleeware, postsController.createNewPost);
+router.post("/", authMiddleware, postsController.createNewPost);
 //게시글 상세 조회
 router.get("/:postId", postsController.getPostDetail);
 //게시글 수정
