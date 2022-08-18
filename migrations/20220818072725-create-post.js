48a250a7e267d527d06a22503b5f5fafe13d1a82
rmadbstjd <rmadbstjd@naver.com>
2022-08-18 16:29:16 +0900

    post table images 추가

--- migrations/20220818072725-create-post.js
+++ migrations/20220818072725-create-post.js
@@ -24,6 +24,9 @@ module.exports = {
         type: Sequelize.INTEGER,
         defaultValue: 0,
       },
+      Images: {
+        type: Sequelize.STRING,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
